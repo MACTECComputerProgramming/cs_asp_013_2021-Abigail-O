9aa4f92ca442c0808b43a45f0e04f87fa611e889
Abigail-O <oabi2305@gmail.com>
2021-09-22 14:00:08 -0500

    second push(idk what im doing)

---
+++ CS_ASP_013-Abigail-O/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_ASP_013_Abigail_O
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (wheatButton.IsChecked == true)
+            {
+                sandwhichLabel.Content = "Wheat,";
+            }
+            else if (italianButton.IsChecked == true)
+            {
+                sandwhichLabel.Content = "Italian,";
+            }
+            else if (flatButton.IsChecked == true)
+            {
+                sandwhichLabel.Content = "Flat Bread,";
+            }
+            if (hamButton.IsChecked == true)
+            {
+                sandwhichLabel.Content = " Ham, ";
+            }
+            else if (chickenButton.IsChecked == true)
+            {
+                sandwhichLabel.Content = " Roasted Chicken, ";
+            }
+            else if (meatballButton.IsChecked == true)
+            {
+                sandwhichLabel.Content = " Meatballs, ";
+            }
+            else if (steakButton.IsChecked == true)
+            {
+                sandwhichLabel.Content = " Steak, ";
+            }
+
+
+
+
+        }
+    }
+}
